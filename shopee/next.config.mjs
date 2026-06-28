abfe42f21f1ce1cc84a1de53a570b562c408aef4
flow11928536-dev <SEUEMAILDOGITHUB>
2026-06-28 15:07:39 -0300

    instala dependencias e configura next para export estatico

--- shopee/next.config.mjs
+++ shopee/next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-    ],
-  },
+  output: 'export',
+  // outras configs...
 };
 
 export default nextConfig;
